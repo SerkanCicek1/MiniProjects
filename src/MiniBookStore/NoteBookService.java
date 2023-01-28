5219ab709521b30de2b5a9fc3d345ea43ad14f02
SerkanCicek1 <serkann017@gmail.com>
2023-01-28 19:18:17 +0300

    Fifth Week

---
+++ src/MiniBookStore/NoteBookService.java
@@ -0,0 +1,29 @@
+package MiniBookStore;
+//2-b-ProductService i implement et ve metodları notebook için doldur
+//ödev:notebook için uygun şekilde doldurunu
+public class NotebookService implements ProductService{
+    @Override
+    public void processMenu() {
+
+    }
+
+    @Override
+    public void listProduct() {
+
+    }
+
+    @Override
+    public void addProduct() {
+
+    }
+
+    @Override
+    public void removeProduct() {
+
+    }
+
+    @Override
+    public void filterProduct(String filter) {
+
+    }
+}
---
+++ src/MiniBookStore/ProductService.java
@@ -0,0 +1,15 @@
+package dt.MiniBookStore;
+//2-product işlemleri
+public interface ProductService {
+
+    void processMenu();
+
+    void listProduct();
+
+    void addProduct();
+
+    void removeProduct();
+
+    void filterProduct(String filter);
+
+}
